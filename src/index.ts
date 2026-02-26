bb9dc5b5174ab5ab9ec31016f37484394cd342ce
Abilash N <abilashnarayanan2001@gmail.com>
2026-02-26 12:08:55 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import routes from "./routes.js";
+import cors from "cors";
+import { v2 as cloudinary } from "cloudinary";
+
+const app = express();
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use("/api/utils", routes);
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Utils service is running on port http://localhost:${process.env.PORT}`,
+  );
+});
